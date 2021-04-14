48bc1ca98ffe8ecfd41f46558f700c699ec560a8
Think Link <software@thinklink.com.co>
2021-04-13 22:15:07 -0500

    Changing MySQL user access

--- db/database.js
+++ db/database.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'piloto2021',
+    user: 'pilot',
+    password: 'smart2021',
     database: 'agroriego',
     multipleStatements: true
 });
