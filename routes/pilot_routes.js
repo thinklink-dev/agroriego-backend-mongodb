004cd75fc9a7662df4ea4312a73410df2e382d12
Think Link <software@thinklink.com.co>
2021-04-16 09:40:36 -0500

    New pilot route to POST

--- routes/pilot_routes.js
+++ routes/pilot_routes.js
@@ -1,23 +1,45 @@
 const express = require('express');
+const pool = require('../db/mariadb.js');
 const router = express.Router();
 const mysqlConnection = require('../db/mariadb.js');
 
 // Insert Sensor Humidity Data
-router.post('/pilot', (req, res) => {
-    let data = {
-        nodo: req.body.nodo,
-        humedad: req.body.humedad
+// router.post('/pilot', (req, res) => {
+//     let data = {
+//         nodo: req.body.nodo,
+//         humedad: req.body.humedad
+//     };
+//     let sql = "INSERT INTO pilots SET ?";
+//     mysqlConnection.query(sql, data, (err,  results) => {
+//         if (err) throw err;
+//         res.send(JSON.stringify({
+//             "status": 200,
+//             "error": null,
+//             "response": results
+//         }));
+//     });
+// });
+
+
+// POST Sensor Data
+router.post('/pilot', async function(req, res) {
+  try {
+
+    const data = {
+      nodo: req.body.nodo,
+      humedad: req.body.humedad
     };
-    let sql = "INSERT INTO pilots SET ?";
-    mysqlConnection.query(sql, data, (err,  results) => {
-        if (err) throw err;
-        res.send(JSON.stringify({
-            "status": 200,
-            "error": null,
-            "response": results
-        }));
-    });
+    const sqlQuery = 'INSERT INTO pilots SET ?';
+    const result = await pool.query(sqlQuery, data);
+    res.status(200).json({ userId: result.insertId });
+
+  } catch (error) {
+    res.status(400).send(error.message);
+    
+  }
 });
+
+
 
 // GET All data from Sensor Humidity
 router.get('/pilot', (req, res) => {
