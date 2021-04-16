7ab7b6a9acb8e95203fdfe2abd1d7dcf69cd16d0
Think Link <software@thinklink.com.co>
2021-04-15 20:46:24 -0500

    Setting root password

--- db/mariadb.js
+++ db/mariadb.js
@@ -3,7 +3,7 @@ const mariadb = require('mariadb');
 const pool = mariadb.createPool({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'piloto2021',
     database: 'agroriego',
     connectionLimit: 5
 });
