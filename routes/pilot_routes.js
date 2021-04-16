d10c889a3b17b5dd8662ebc2501fd57382c1d09a
Think Link <software@thinklink.com.co>
2021-04-16 17:33:58 -0500

    Setting GET Route

--- routes/pilot_routes.js
+++ routes/pilot_routes.js
@@ -39,14 +39,29 @@ router.post('/pilot', async function(req, res) {
 
 
 // GET All data from Sensor Humidity
-router.get('/pilot', (req, res) => {
-    mysqlConnection.query('SELECT * FROM pilots', (err, rows, fields) => {
-      if(!err) {
-        res.json(rows);
-      } else {
-        console.log(err);
-      }
-    });  
-  });
+// router.get('/pilot', (req, res) => {
+//     mysqlConnection.query('SELECT * FROM pilots', (err, rows, fields) => {
+//       if(!err) {
+//         res.json(rows);
+//       } else {
+//         console.log(err);
+//       }
+//     });  
+//   });
+
+// GET Sensor Data
+router.get('/pilot', async function (req, res) {
+  try {
+    
+    const sqlQuery = 'SELECT * FROMN pilots';
+    const rows = await pool.query(sqlQuery);
+    res.status(200).json(rows);
+
+  } catch (error) {
+    
+    res.status(400).send(error.message);
+    
+  }
+});
   
   module.exports = router;
