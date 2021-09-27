3411b3cb5a3aa3d588b1c3a01d0f11a91afc9162
Think Link <software@thinklink.com.co>
2021-09-27 15:45:04 -0500

    Updating routes and fields

--- routes/pilot_routes.js
+++ routes/pilot_routes.js
@@ -1,33 +1,22 @@
 const express = require('express');
 const pool = require('../db/mariadb.js');
 const router = express.Router();
-const mysqlConnection = require('../db/mariadb.js');
-
-// Insert Sensor Humidity Data
-// router.post('/pilot', (req, res) => {
-//     let data = {
-//         nodo: req.body.nodo,
-//         humedad: req.body.humedad
-//     };
-//     let sql = "INSERT INTO pilots SET ?";
-//     mysqlConnection.query(sql, data, (err,  results) => {
-//         if (err) throw err;
-//         res.send(JSON.stringify({
-//             "status": 200,
-//             "error": null,
-//             "response": results
-//         }));
-//     });
-// });
-
+// const mysqlConnection = require('../db/mariadb.js');
 
 // POST Sensor Data
 router.post('/pilot', async function(req, res) {
   try {
+    
+    const riego = "";
+    if (humedad <= 50) {
+      riego = "DEBE regar el cultivo";
+    } else {
+      riego = "No es necesario regar el cultivo"
+    }
 
-    const { nodo, humedad } = req.body;
-    const sqlQuery = 'INSERT INTO pilots (nodo, humedad) VALUES (?,?)';
-    const result = await pool.query(sqlQuery, [nodo, humedad]);
+    const { nodo, humedad, riego } = req.body;
+    const sqlQuery = 'INSERT INTO pilots (nodo, humedad, recomendacion) VALUES (?,?,?)';
+    const result = await pool.query(sqlQuery, [nodo, humedad, recomendacion]);
     res.status(200).json({ userId: result.insertId });
 
   } catch (error) {
@@ -36,18 +25,6 @@ router.post('/pilot', async function(req, res) {
   }
 });
 
-
-
-// GET All data from Sensor Humidity
-// router.get('/pilot', (req, res) => {
-//     mysqlConnection.query('SELECT * FROM pilots', (err, rows, fields) => {
-//       if(!err) {
-//         res.json(rows);
-//       } else {
-//         console.log(err);
-//       }
-//     });  
-//   });
 
 // GET Sensor Data
 router.get('/pilot', async function (req, res) {
@@ -63,5 +40,35 @@ router.get('/pilot', async function (req, res) {
     
   }
 });
+
+// GET Sensor Data - Last 2 values
+router.get('/pilot-2', async function (req, res) {
+  try {
+    
+    const sqlQuery = 'SELECT * FROM pilots ORDER BY date DESC LIMIT 2';
+    const rows = await pool.query(sqlQuery);
+    res.status(200).json(rows);
+
+  } catch (error) {
+    
+    res.status(400).send(error.message);
+    
+  }
+});
+
+// GET Sensor Data - Last 6 values
+router.get('/pilot-6', async function (req, res) {
+  try {
+    
+    const sqlQuery = 'SELECT * FROM pilots ORDER BY date DESC LIMIT 6';
+    const rows = await pool.query(sqlQuery);
+    res.status(200).json(rows);
+
+  } catch (error) {
+    
+    res.status(400).send(error.message);
+    
+  }
+});
   
-  module.exports = router;
+module.exports = router;
