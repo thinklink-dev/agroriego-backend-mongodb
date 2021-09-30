0943363bef6a333d99bbba92bc750ebdd590d38e
Think Link <software@thinklink.com.co>
2021-09-29 21:42:39 -0500

    Adding CORS middleware

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 // Middlewares
 app.set('port', process.env.PORT || 3000);
 app.use(express.json());
+app.use(cors());
+app.options('*', cors());
 
 // Routes
 app.use(require('./routes/pilot_routes.js'));
